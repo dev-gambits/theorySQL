bc6022efa1d68bf0c177574bb5496a1a1e1c8525
Emmanuel Nieto M <emmanuel.nieto.munoz@ciisa.cl>
2024-03-29 23:06:35 -0300

    Add SQL and Documenattion POSTGRESQL

---
+++ sql/queries_native.sql
@@ -0,0 +1,492 @@
+-- QUERY'S NATIVE NAME_PROJECT
+
+-- QUERY'S NATIVE Nº 1
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 2
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE  Nº 3
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 4
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 5
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 6
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 7
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 8
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 9
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 10
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 11
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 12
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 13
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 14
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 15
+-- Description:  
+
+
+
+
+
+-- QUERY'S NATIVE Nº 16
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 17
+-- Description:  
+
+
+
+
+
+-- QUERY'S NATIVE Nº 18
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 19
+-- Description:  
+
+
+
+
+
+-- QUERY'S NATIVE Nº 20
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 21
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 22
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE  Nº 23
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 24
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 25
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 26
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 27
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 28
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 29
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 30
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 31
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 32
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 33
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 34
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 35
+-- Description:  
+
+
+
+
+
+-- QUERY'S NATIVE Nº 36
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 37
+-- Description:  
+
+
+
+
+
+-- QUERY'S NATIVE Nº 38
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 39
+-- Description:  
+
+
+
+
+
+-- QUERY'S NATIVE Nº 40
+-- Description:  
+
+
+
+
+
+-- QUERY'S NATIVE Nº 41
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 42
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE  Nº 43
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 44
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 45
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 46
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 47
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 48
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 49
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 50
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 51
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 52
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 53
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 54
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 55
+-- Description:  
+
+
+
+
+
+-- QUERY'S NATIVE Nº 56
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 57
+-- Description:  
+
+
+
+
+
+-- QUERY'S NATIVE Nº 58
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 59
+-- Description:  
+
+
+
+
+
+-- QUERY'S NATIVE Nº 60
+-- Description:  
+
+
+
+
+
+-- QUERY'S NATIVE Nº 61
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 62
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE  Nº 63
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 64
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 65
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 66
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 67
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 68
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 69
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 70
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 71
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 72
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 73
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 74
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 75
+-- Description:  
+
+
+
+
+
+-- QUERY'S NATIVE Nº 76
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 77
+-- Description:  
+
+
+
+
+
+-- QUERY'S NATIVE Nº 78
+-- Description:  
+
+
+
+
+-- QUERY'S NATIVE Nº 79
+-- Description:  
+
+
+
+
+
+-- QUERY'S NATIVE Nº 80
+-- Description:  
---
+++ sql/test_04.sql
@@ -0,0 +1,125 @@
+-- QUERY'S NAME_PROJECT
+
+-- QUERY'S Nº 1
+-- Description:  
+
+
+
+
+-- QUERY'S Nº 2
+-- Description:  
+
+
+
+
+-- QUERY'S Nº 3
+-- Description:  
+
+
+
+
+-- QUERY'S Nº 4
+-- Description:  
+
+
+
+
+-- QUERY'S Nº 5
+-- Description:  
+
+
+
+
+-- QUERY'S Nº 6
+-- Description:  
+
+
+
+
+-- QUERY'S Nº 7
+-- Description:  
+
+
+
+
+-- QUERY'S Nº 8
+-- Description:  
+
+
+
+
+-- QUERY'S Nº 9
+-- Description:  
+
+
+
+
+-- QUERY'S Nº 10
+-- Description:  
+
+
+
+
+-- QUERY'S Nº 11
+-- Description:  
+
+
+
+
+-- QUERY'S Nº 12
+-- Description:  
+
+
+
+
+-- QUERY'S Nº 13
+-- Description:  
+
+
+
+
+-- QUERY'S Nº 14
+-- Description:  
+
+
+
+
+-- QUERY'S Nº 15
+-- Description:  
+
+
+
+
+
+-- QUERY'S Nº 16
+-- Description:  
+
+
+
+
+-- QUERY'S Nº 17
+-- Description:  
+
+
+
+
+
+-- QUERY'S Nº 18
+-- Description:  
+
+
+
+
+-- QUERY'S Nº 19
+-- Description:  
+
+
+
+
+
+-- QUERY'S Nº 20
+-- Description:  
+
+
+
+
